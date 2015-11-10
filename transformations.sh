e3aeb90d4bc291dcdd226de993f3b8d91f50618b
Jonny Huck <jonnyhuck@gmail.com>
2015-11-10 16:18:40 +0000

    Update transformations.sh
    corrected some errors in the mapnik commmand

--- transformations.sh
+++ transformations.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-#calculate the points
+#calculate the grid of lat lng points
 python generateData.py
 
 #run the transformations
@@ -9,7 +9,7 @@ ogr2ogr -f "ESRI Shapefile" -s_srs EPSG:4326 -t_srs "+proj=tmerc +lat_0=49 +lon_
 ogr2ogr -f "ESRI Shapefile" -t_srs "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs +nadgrids=/Users/jonnyhuck/Downloads/ostn02-ntv2-data/OSTN02_NTv2.gsb" -s_srs EPSG:4326 -overwrite ostn02.shp grid.vrt
 
 # render using Mapnik (UK Example)
-# nik2img.py OSNT02Blend.xml gb.png --srs 27700 --dimensions 700 1300 0 0 700000 1300000 -w pgw
+# nik2img.py OSNT02Blend.xml gb.png -s 27700 -d 700 1300 -e 0 0 700000 1300000 -w pgw
 
 # render using Mapnik (Stornoway Example)
-nik2img.py OSNT02Blend.xml gb.png --srs 27700 --dimensions 500 500 142500 933000 142600 933100 -w pgw
+nik2img.py OSNT02Blend.xml gb.png -s 27700 -d 500 500 -e 142500 933000 142600 933100 -w pgw
